03da5701fe0771a9f9d9381f1364f9a8b91139b7
Jyeon-H <wldusakak2@gmail.com>
2025-07-10 20:14:57 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from yolo import train_yolov5
+from cnn import train_cnn
+from mlp import train_mlp
+from plot_mis import plot_misclassified
+
+def main():
+    print("[1단계] YOLOv5로 계량기 탐지 학습")
+    train_yolov5()
+    
+    print("[2단계] MLP 모델로 숫자 인식 학습")
+    mlp, x_test_mlp, y_test_mlp = train_mlp()
+    plot_misclassified(mlp, x_test_mlp, y_test_mlp)
+
+    print("[3단계] CNN 모델로 숫자 인식 학습")
+    cnn, x_test_cnn, y_test_cnn = train_cnn()
+    plot_misclassified(cnn, x_test_cnn, y_test_cnn)
+    
+if __name == '__main__':
+    main()
