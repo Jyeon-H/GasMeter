2f08dd4852b6572e4513c38313436439740fb4c1
Jyeon-H <wldusakak2@gmail.com>
2025-07-10 20:01:51 +0900

    Update mlp.py

--- model/mlp.py
+++ model/mlp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from data import load_data
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
@@ -7,23 +8,16 @@ from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
 
 def train_mlp(x, y, epochs=30, batch_size=128):
-
+    x, y = load_data()
     x = x.reshape(-1, 28*28).astype(np.float32) / 255.0
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
     y_train = to_categorical(y_train, 10)
     y_test = to_categorical(y_test, 10)
 
-
-    n_input = 784
-    n_hidden = 1024
-    n_output = 10
-
     model = Sequential([
-    Dense(1024, activation='tanh', input_shape=(784,), 
-    kernel_initializer='random_uniform', bias_initializer='zeros'),
-    Dense(10, activation='tanh', 
-    kernel_initializer='random_uniform', bias_initializer='zeros')
+    Dense(1024, activation='tanh', input_shape=(784,), kernel_initializer='random_uniform', bias_initializer='zeros'),
+    Dense(10, activation='tanh', kernel_initializer='random_uniform', bias_initializer='zeros')
     ])
     
     model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
